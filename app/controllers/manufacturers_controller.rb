bbd3ddd061adcab1606a1c3c9642454ffee90404
Namae-no-nai <guilhermebluiz@hotmail.com>
2020-04-08 19:24:07 -0300

    Cadastrar Fabricante + fix

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -6,4 +6,23 @@ class ManufacturersController < ApplicationController
   def show
     @manufacturer = Manufacturer.find(params[:id])
   end
+
+  def new
+    @manufacturer = Manufacturer.new
+  end
+
+  def create
+    @manufacturer = Manufacturer.create(manufacturer_params)
+    if @manufacturer.save
+      redirect_to @manufacturer
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def manufacturer_params
+    params.require(:manufacturer).permit(:name)
+  end
 end
