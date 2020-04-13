dea7b9c9a0f691bec1bc15dbd36aeaf6628992a4
Namae-no-nai <guilhermebluiz@hotmail.com>
2020-04-13 15:29:04 -0300

    arrumando rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
-  resources :manufacturers
-  resources :subsidiaries
-  resources :car_categories
-  resources :clients
+  resources :manufacturers, only: [:index, :new, :create, :show]
+  resources :subsidiaries, only: [:index, :new, :create, :show, :update, :edit]
+  resources :car_categories, only: [:index, :new, :create, :show]
+  resources :clients, only: [:index, :new, :create, :show]
 end
