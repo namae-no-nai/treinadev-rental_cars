42c5b26d5f3c4975f78c1f38519b8c05ae9d116c
Namae-no-nai <guilhermebluiz@hotmail.com>
2020-04-24 21:53:16 -0300

    visitante ve todos os carros da categoria

--- spec/features/car_category/admin_view_car_category_spec.rb
+++ spec/features/car_category/admin_view_car_category_spec.rb
@@ -56,4 +56,24 @@ feature 'Admin view subsidiaries' do
 
     expect(current_path).to eq car_categories_path
   end
+
+  scenario 'view all cars' do
+    fiat = Manufacturer.create!(name: 'Fiat')
+    cat_a = CarCategory.create!(name: 'A',  daily_rate: '12.22', car_insurance: '15.99', third_party_insurance: '33.33')
+    uno = CarModel.create!(name:'Uno', year: 2020, manufacturer: fiat, motorization: "1.0", car_category: cat_a, fuel_type: "flex")
+    mobi = CarModel.create!(name:'Mobi', year: 2020, manufacturer: fiat, motorization: "1.0", car_category: cat_a, fuel_type: "flex")
+
+    visit root_path
+    click_on "Categorias de carros"
+    click_on "A"
+
+    expect(page).to have_content('Categoria: A')
+    expect(page).to have_content('12,22')
+    expect(page).to have_content('15,99')
+    expect(page).to have_content('33,33')
+    expect(page).to have_link('Uno', href: car_model_path(uno))
+    expect(page).to have_link('Mobi', href: car_model_path(mobi))
+    expect(page).to have_link('Voltar', href: car_categories_path)
+
+  end
 end
